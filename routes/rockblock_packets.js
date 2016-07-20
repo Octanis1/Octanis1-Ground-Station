8a9c81e287f470bb0f991b3fe7a1fda98cddef3a
Joseph LEFEVRE <padawan_joseph@yahoo.fr>
2016-07-20 13:56:45 +0000

    add get rockblockpacket/data to get only data of the packets

--- routes/rockblock_packets.js
+++ routes/rockblock_packets.js
@@ -41,65 +41,24 @@ router.post('/'+ process.env.GATEWAY_KEY, xmlParser({trim: false, explicitArray:
     if(err) throw err;
   });
   
-  /*
-   req.body is sent back correctly here. Tested with :
-
-    <?xml version="1.0" encoding="UTF-8" ?>
-    <packet>
-        <imei>321</imei>
-        <momsn>432</momsn>
-        <transit_time>12-10-10 10:41:50</transit_time>
-        <iridium_latitude>9</iridium_latitude>
-        <iridium_longitude>9</iridium_longitude>
-        <iridium_cep>9</iridium_cep>
-        <data>08befdfaf5fbffffffff011500802e441dbebebebe20befdfaf5fbffffffff01280530befdfaf5fbffffffff01380540befdfaf5fbffffffff01</data>
-    </packet>*/
-  /* // below what you need if you recieve a text/plain.
-  parser.parseString(req.body.toString(), function(err,result){
-    //Extract the value from the data element
-    imei = result['packet']['imei'];
-    momsn = result['packet']['momsn'];
-    transit_time = result['packet']['transit_time'];
-    
-    var year='20'.concat(transit_time[0][0],transit_time[0][1]);
-    var month=transit_time[0][3].concat(transit_time[0][4]);
-    var day=transit_time[0][6].concat(transit_time[0][7]);
-    var hour=transit_time[0][8].concat(transit_time[0][9]);
-    var minute=transit_time[0][11].concat(transit_time[0][12]);
-    var second=transit_time[0][14].concat(transit_time[0][15]);
-    dt = new Date(year,month,day,hour,minute,second);
-
-    iridium_latitude = result['packet']['iridium_latitude'];
-    iridium_longitude = result['packet']['iridium_longitude'];
-    iridium_cep = result['packet']['iridium_cep'];
-    data = result['packet']['data'];
-    
-  });
-  
-  var newRockblockPacket_frame = {  
-    imei: parseFloat(imei),
-    momsn: parseFloat(momsn),
-    transit_time: dt, //transit_time,
-    iridium_latitude: parseFloat(iridium_latitude),
-    iridium_longitude: parseFloat(iridium_longitude),
-    iridium_cep: parseFloat(iridium_cep),
-    data: data
-  }
-
-  var newRockblockPacket = RockblockPacket(newRockblockPacket_frame);
-
-//  console.log("imei :".concat(imei.toString(),"\nmomsn :",momsn.toString(),"\niridium_latitude :", iridium_latitude.toString(), "\niridium_cep :", iridium_cep.toString(),"\ndata :",data.toString()))
-
-  newRockblockPacket.save(function(err){
-    if(err) throw err;
-  });*/
-  
   res.status(200).send('OK');
 });
 
 /* GET */
-router.get('/'+ process.env.GATEWAY_KEY, function(req, res, next) {
+router.get('/data/'+ process.env.GATEWAY_KEY, function(req, res, next) {
+  var final="";
+  RockblockPacket.find({}, function(err, docs) {
+    if (!err){
+        docs.forEach(function(item,index){
+          final=final.concat(String(item.data),"<br/>");
+        });
+        res.send(final);
+    } else {throw err;}
+  });
 
+});
+
+router.get('/'+ process.env.GATEWAY_KEY, function(req, res, next) {
   RockblockPacket.find({}, function(err, docs) {
     if (!err){
         res.send(docs);
@@ -107,7 +66,5 @@ router.get('/'+ process.env.GATEWAY_KEY, function(req, res, next) {
   });
 
 });
-
-
 
 module.exports = router;
